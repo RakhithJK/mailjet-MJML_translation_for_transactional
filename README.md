e897a2e61741b2c4175f5a0d04b4ab782949de87
Emmanuel Boisgontier <eboisgontier@mailjet.com>
2017-04-27 16:02:49 +0300

    PHP comments + Readme

--- README.md
+++ README.md
@@ -4,3 +4,46 @@ This POC will show how to leverage MJML to simply manage advanced transactional 
 
 ![](Flow.png)
 
+This sample leverage (MJML)[https://mjml.io], the only framework that makes responsive email easy, to ease the management of reusable piece of responsive templates.
+
+The step covered by this sample are as follow : 
+
+ - Processing of MJML to get a HTML email
+ - Substitution of translation placeholders with a database of transalation (to make it easier here we are using PHP files to store the different languages)
+ - Creation or update of template through Mailjet API 
+
+This project contain several folders : 
+
+ - [input](https://github.com/eboisgon/MJML_translation_for_transactional/tree/master/input) : all the MJML files used to represent the final messages
+ - [output](https://github.com/eboisgon/MJML_translation_for_transactional/tree/master/output) : result of MJML processing (still contain the translation placeholders)
+ - [output_trans](https://github.com/eboisgon/MJML_translation_for_transactional/tree/master/output_trans) : contain the final templates that will be stored on Mailjet
+ - [trans](https://github.com/eboisgon/MJML_translation_for_transactional/tree/master/trans) : contain the translation indexes, one file per language. 
+
+## Use of MJML 
+
+To mutualise some part of the template (header, footer, snippets), we are leveraging here the use of the `<MJ-INCLUDE>` MJML tag. It allows to load external files to build you email template. (see (here)[https://mjml.io/documentation/#mj-include] for more information) 
+
+As a convention all headers, footers and snippets name start with a `_` and will not be directly processed by the script. 
+
+##Install and Run 
+
+To install [Mailjet PHP wrapper](https://github.com/mailjet/mailjet-apiv3-php) : 
+
+`composer install`
+
+To install the MJML CLI :
+
+`npm install mjml`
+
+More details [here](https://mjml.io/documentation/#installation) 
+
+Configuration in conf.php 
+
+ - set your Mailjet API keys 
+ - set your the command line for the MJML CLI in `$path_mjml`
+
+Next just need to run 
+
+`php process.php`
+
+
--- process.php
+++ process.php
@@ -6,39 +6,54 @@ require_once("conf.php");
 use \Mailjet\Resources;
 
 $list_input = scandir($dir_input);
-
 $list_htmls = [];
 $list_htmls_final = [];
 
 
+/* PART 1 : 
+	recover the MJML files and process 
+
+*/
 
 foreach ($list_input as $file){
 	if(!preg_match("/^[\._]/i",$file)){
+		//generate command line 		
 		$command=str_replace("[file_input]", $file , $path_mjml);
 		$command=str_replace("[file_output]", str_replace(".mjml", ".html" , $file) , $command);
 		$list_htmls[]=str_replace(".mjml", ".html" , $file);
-		print($command."\n");
+		//print($command."\n");
 		
 		echo exec($command,$output);
 		print_r($output);
 	}
 }
+//list of html template generated pre-translation
+//print_r($list_htmls);
 
+
+/* PART 2 : 
+	transalation of the HTML files 
+*/
+
+//find the translation files 
 $list_trans = scandir($dir_trans);
 
-print_r($list_htmls);
+
 foreach ($list_trans as $file){
 	if(!preg_match("/^[\._]/i",$file)){
+		//load translation from file		
 		require_once($dir_trans.$file);
-		print($dir_trans.$file." - ".$lg."\n");
-		print_r($trans);
+		print("TRANLATION ".$dir_trans.$file." - ".$lg."\n");
+		//print_r($trans);
 		foreach ($list_htmls as $html){
 			$content = file_get_contents ($dir_output.$html);
+			//replace all [[ ]] by translation 
 			if (preg_match_all("/\[\[(.*?)\]\]/", $content, $m)) {
 			      foreach ($m[1] as $i => $varname) {
 				$content = str_replace($m[0][$i], sprintf('%s', $trans[$varname]), $content);
 			      }
 			}		
+			//translated html saved as $filename_$lg.html from $filename.html  
 			$file_name=str_replace(".html", "_".$lg.".html" , $html);
 			$template_name=str_replace(".html","" , $file_name);	
 			print("SAVE : ".$dir_output_trans.$file_name."\n");
@@ -48,8 +63,12 @@ foreach ($list_trans as $file){
 	}
 }
 
+//list of all the template to send to Mailjet
 print_r($list_htmls_final);
 
+/* PART 3 : 
+	transfer by API of all template to Mailjet for use as transactional templates
+*/
 
 $mj = new \Mailjet\Client($API_KEY, $API_SECRET_KEY);
 
@@ -57,6 +76,7 @@ foreach ($list_htmls_final as $template_name => $file){
 	print("Template ".$template_name."\n");
 	$response = $mj->get(Resources::$Template, ['id' => "apikey|".$template_name]);
 	if(!$response->success()){
+		//template not yet created on Mailjet
 		print($response->getReasonPhrase()."\n");
 		print("$template_name not found on Mailjet : Let's create it !!\n");	
 		$body = [
@@ -68,6 +88,7 @@ foreach ($list_htmls_final as $template_name => $file){
 		$response = $mj->post(Resources::$Template, ['body' => $body]);
 		$response->success() && var_dump($response->getData());	
 	} 
+	//add html content to the template
 	$content = file_get_contents ($dir_output_trans.$file);
 	$body = [
 	    'Html-part' => $content,
@@ -75,6 +96,7 @@ foreach ($list_htmls_final as $template_name => $file){
 	//print_r($body);
 	$response = $mj->post(Resources::$TemplateDetailcontent, ['id' => "apikey|".$template_name, 'body' => $body]);
 	if(!$response->success()){
+		//error
 		print($response->getReasonPhrase()."\n");
 	}	
 }
